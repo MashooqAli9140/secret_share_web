268b2c9f84c79aba5e5f7d30acfbb8406d2c7076
MashooqAli9140 <Faizsde9140@gmail.com>
2024-11-28 14:40:21 +0530

    changes in get req

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ app.get("/get-secret" , async (req , res ) => {
 try {    
 const Getallthesecrets = await NewSecretdata.find();
  console.log("all secrets are here",Getallthesecrets )
- res.status(200).json( { data: Getallthesecrets , msge:"fecthing the secrets"})
+ res.status(200).json( {data: Getallthesecrets})
 
 } catch (error) {
     console.log( error,"error while fetching the data");
